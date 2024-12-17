65be8303e43642175bf52aa2e3850de66ca938cd
joj <young2k15@tutanota.com>
2024-12-17 08:36:38 -0500

    first test

---
+++ ecs-deployment.ts
@@ -0,0 +1,117 @@
+import { App } from 'cdktf';
+import { BaseStackProps } from './lib/stacks/stackbase';
+import { taskDefinitionStack } from './lib/stacks/taskdefinitions-stack';
+import { LoadBalancerStack, LbConfigs } from './lib/stacks/loadbalancer-stack';
+import { EcsClusterStack } from './lib/stacks/ecs-cluster-stack';
+import { EcsServiceStack, EcsServiceConfigs } from './lib/stacks/ecs-service-stack';
+import { LaunchTemplateStack, LaunchTemplateConfigs } from './lib/stacks/launchtemplate-stack';
+import { AutoScalingStack, AutoScalingConfigs } from './lib/stacks/autoscaling-stack';
+import { AppAutoScalingStack, AppAutoScalingConfigs } from './lib/stacks/application-as-stack';
+import { sgStack } from './lib/stacks/securitygroup-stack';
+
+const StackProps: BaseStackProps = {
+    name: "runner",
+    project: "github-group",
+    region: "us-east-2"
+}
+
+function aFile(key: string){
+    const fileS = require('fs');
+    fileS.writeFileSync('./scripts/cluster.sh',"#!/bin/bash\n");
+    fileS.appendFileSync('./scripts/cluster.sh',"sudo echo ECS_CLUSTER=" + key + " >> /etc/ecs/ecs.config");
+}
+
+const app = new App();
+const cluster = new EcsClusterStack(app, "ecs-cluster-stack", StackProps);
+const sGroup = new sgStack(app, "sg-stack", StackProps);
+
+const clusterName = `${StackProps.name}-${StackProps.project}-cluster`;
+aFile(clusterName);
+
+const LbConfig: LbConfigs = {
+    name: StackProps.name,
+    project: StackProps.project,
+    region: StackProps.region,
+    securityGroup: sGroup.sg.id,
+    certificate: `${process.env.CERTIFICATE}`,
+}
+
+const LTConfig: LaunchTemplateConfigs = {
+    name: StackProps.name,
+    project: StackProps.project,
+    region: StackProps.region,
+    imageId: "ami-00f453db4525939cf",
+    instanceType: "t3.micro",
+    iamInstanceProfile: cluster.instanceProfile.name,
+    securityGroupIds: [sGroup.sg.id],
+    userData: "./scripts/cluster.sh"
+}
+
+const launchTemplate = new LaunchTemplateStack(app, "lt-stack", LTConfig)
+
+const AsgConfig: AutoScalingConfigs = {
+    name: StackProps.name,
+    project: StackProps.project,
+    region: StackProps.region,
+    desiredCapacity: 1,
+    minSize: 1,
+    maxSize: 3,
+    launchTemplate: {
+        id: launchTemplate.launchTemplate.id,
+    },
+    vpcZoneIdentifier: [`${process.env.SUBNET}`, `${process.env.SUBNET_2}`],
+    cpuTargetValue: 80,
+    memoryTargetValue: 80,
+    ecsClusterName: cluster.cluster.name,
+}
+
+new AutoScalingStack(app, "asg-stack", AsgConfig)
+
+/*const InstanceConfig: InstanceConfigs {
+    launchTemplate: {
+        id: launchTemplate.launchTemplate.id
+}*/
+
+const taskDefinition = new taskDefinitionStack(app, "td-stack", StackProps);
+const lb = new LoadBalancerStack(app, "lb-stack", LbConfig);
+
+const EcsConfig: EcsServiceConfigs = {
+    name: StackProps.name,
+    project: StackProps.project,
+    region: StackProps.region,
+    cluster: cluster.cluster.arn,
+    taskDefinition: taskDefinition.td.arn,
+    targetGroup: lb.targetGroup.arn,
+    securityGroup: sGroup.sg.id,
+    desiredCount: 1,
+}
+
+const ecs = new EcsServiceStack(app, "ecs-service-stack", EcsConfig);
+
+const AppAsConfig : AppAutoScalingConfigs = {
+    name: StackProps.name,
+    project: StackProps.project,
+    region: StackProps.region,
+    minCapacity: 1,
+    maxCapacity: 4,
+    cpuTargetValue: 70,
+    memoryTargetValue: 70,
+    ecsClusterName: cluster.cluster.name,
+    ecsServiceName: ecs.ecs.name
+}
+
+new AppAutoScalingStack(app, "ecs-autoscaling-stack", AppAsConfig)
+
+// To deploy using Terraform Cloud comment out the above line
+// And uncomment the below block of lines
+
+/*const stack = new EcsServiceStack(app, "ecs-service-stack", EcsConfig);
+new RemoteBackend(stack, {
+  hostname: "app.terraform.io",
+  organization: process.env.CDKTF_ECS_TFC_ORGANIZATION || "",
+  workspaces: {
+    name: "ecs-microservices-cdktf"
+  }
+}); */
+
+app.synth();
---
+++ lib/stacks/codebuild-stack.ts
@@ -0,0 +1,99 @@
+import { Construct } from 'constructs';
+import { AwsStackBase } from './stackbase';
+import { IamRole } from '@cdktf/provider-aws/lib/iam-role';
+import { CodebuildProject } from '@cdktf/provider-aws/lib/codebuild-project'
+import { CodebuildWebhook } from '@cdktf/provider-aws/lib/codebuild-webhook'
+import { CodebuildSourceCredential } from '@cdktf/provider-aws/lib/codebuild-source-credential'
+
+export class CodebuildStack extends AwsStackBase {
+    public credential: CodebuildSourceCredential;
+    public webhook: CodebuildWebhook;
+    constructor(scope: Construct, id: string, props: BaseStackProps) {
+        super(scope, `${props.name}-${id}`, {
+            name: `${props.project}`,
+            project: `${props.project}`,
+            region: `${props.region}`
+        })
+        const codebuildRole = new IamRole(this, `${props.name}-ecs-role`, {
+
+          name: `${props.name}-codebuild-role`,
+          inlinePolicy: [
+            {
+              name: "codebuild",
+              policy: JSON.stringify({
+                Version: "2012-10-17",
+                Statement: [
+                  {
+                    Effect: "Allow",
+                    Action: ["codebuild:*", "ec2:*", "kms:*", "ecr:*"],
+                    Resource: "*",
+                  },
+                ],
+              }),
+            },
+          ],
+          assumeRolePolicy: JSON.stringify({
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Action: "sts:AssumeRole",
+                Effect: "Allow",
+                Sid: "",
+                Principal: {
+                  Service: "codebuild.amazonaws.com",
+                },
+              },
+              {
+                Action: "sts:AssumeRole",
+                Effect: "Allow",
+                Sid: "",
+                Principal: {
+                  Service: "codebuild.amazonaws.com",
+                },
+              },
+            ],
+          }),
+        });
+
+        const codebuild = new CodebuildProject (this, `${props.name}-codebuild-${props.project}`, {
+            name: `${props.name}-${props.project}-codebuild`,
+            artifacts: {type: "NO ARTIFACTS"},
+            serviceRole: codebuildRole.arn,
+            environment: {
+                computeType: "BUILD_GENERAL1_SMALL",
+                image: "aws/codebuild/amazonlinux2-x86_64-standard:4.0",
+                type: "LINUX_CONTAINER",
+            }
+            vpcConfig: [
+                {
+                    securityGroupIds: [`${props.securityGroup}`],
+                    subnets: [`${process.env.SUBNET}`, `${process.env.SUBNET_2}`],
+                    vcpId: `${process.env.VPC_ID}`
+                }
+            ],
+            source: {
+                type: "GITHUB",
+
+            }
+
+        });
+
+        this.webhook = new CodebuildWebhook(this, `${props.name}-${props.project}-webhook`, {
+            projectName: codbuild.name,
+            filterGroup: [
+                filter: [
+                    {
+                        type: "EVENT",
+                        pattern: "WORKFLOW_JOB_QUEUED"
+                    }
+                ]
+            ]
+        });
+
+        this.credential = new CodebuildSourceCredential(this, `${props.name}-${props.project}-credentials`, {
+            authType: "PERSONAL_ACCESS_TOKEN",
+            serverType: "GITHUB",
+            token: `${process.env.GH_TOKEN}`
+        });
+    }
+}
