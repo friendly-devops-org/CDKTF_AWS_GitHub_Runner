51f314a3038e7ca9a0e0360fb6377b6c239b6f93
joj <young2k15@tutanota.com>
2024-12-17 15:22:43 -0500

    location update

--- lib/stacks/codebuild-stack.ts
+++ lib/stacks/codebuild-stack.ts
@@ -78,7 +78,7 @@ export class CodebuildStack extends AwsStackBase {
             },
             source: {
                 type: "GITHUB",
-                location: "https://github.com/friendly-devops-org"
+                location: "CODEBUILD_DEFAULT_WEBHOOK_SOURCE_LOCATION"
 
             }
 
