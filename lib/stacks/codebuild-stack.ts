9ea51b7feb3ac4db800e59c1ee016ab60d0ea5f5
joj <young2k15@tutanota.com>
2024-12-17 19:21:52 -0500

    update roles

--- lib/stacks/codebuild-stack.ts
+++ lib/stacks/codebuild-stack.ts
@@ -32,7 +32,7 @@ export class CodebuildStack extends AwsStackBase {
                 Statement: [
                   {
                     Effect: "Allow",
-                    Action: ["codebuild:*", "ec2:*", "kms:*", "ecr:*"],
+                    Action: ["codebuild:*", "ec2:*", "kms:*", "ecr:*", "cloudwatch:*"],
                     Resource: "*",
                   },
                 ],
