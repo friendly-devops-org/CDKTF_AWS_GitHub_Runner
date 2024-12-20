9942abee14edb6818ed4f6a0a6eb120b04a5277e
joj <young2k15@tutanota.com>
2024-12-19 22:56:56 -0500

    fixed attachment

--- lib/stacks/taskdefinitions-stack.ts
+++ lib/stacks/taskdefinitions-stack.ts
@@ -131,7 +131,8 @@ export class taskDefinitionStack extends AwsStackBase {
                   }
                 ],
                 command: [
-                    "/bin/bash", "-c", "apt install -y curl tar"
+                    "/bin/bash", "-c", "apt install -y curl tar",
+                    "cd ~/",
                     "mkdir actions-runner && cd actions-runner;",
                     " curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz;",
                     "tar xzf ./actions-runner-linux-x64-2.321.0.tar.gz;",
