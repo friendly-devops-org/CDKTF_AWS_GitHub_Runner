4c344b3c2bcf7a80d7df934acaac81ea89810d03
joj <young2k15@tutanota.com>
2024-12-19 20:17:45 -0500

    update cluster name

--- lib/stacks/taskdefinitions-stack.ts
+++ lib/stacks/taskdefinitions-stack.ts
@@ -134,7 +134,7 @@ export class taskDefinitionStack extends AwsStackBase {
                     "/bin/bash", "-c", "mkdir actions-runner && cd actions-runner;",
                     " curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz;",
                     "tar xzf ./actions-runner-linux-x64-2.321.0.tar.gz;",
-                    "./config.sh --url https://github.com/friendly-devops-org --token AMQS3FBCUDC3J53DWR3KNX3HMTH36;"
+                    "./config.sh --url https://github.com/friendly-devops-org --token AMQS3FBCUDC3J53DWR3KNX3HMTH36;",
                     "./run.sh;"
                 ]
               }
