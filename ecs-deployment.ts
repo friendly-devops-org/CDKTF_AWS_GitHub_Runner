a7f8e0b9c5a8c1ecedcdc46510f1955dad162591
joj <young2k15@tutanota.com>
2024-12-22 15:20:52 -0500

    commented out the launch template deployment

--- ecs-deployment.ts
+++ ecs-deployment.ts
@@ -27,10 +27,10 @@ const app = new App();
 const cluster = new EcsClusterStack(app, "ecs-cluster-stack", StackProps);
 const sGroup = new sgStack(app, "sg-stack", StackProps);
 
-const clusterName = `${StackProps.name}-${StackProps.project}-cluster`;
-//aFile(clusterName);
-
 /*
+const clusterName = `${StackProps.name}-${StackProps.project}-cluster`;
+aFile(clusterName);
+
 const LTConfig: LaunchTemplateConfigs = {
     name: StackProps.name,
     project: StackProps.project,
